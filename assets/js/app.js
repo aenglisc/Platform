810c57547df5e543b6922d2f5e112250e852479d
Roman Pushkov <pushkovroman@me.com>
2018-04-16 16:00:37 +0300

    integrating actual games

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
-import Elm from './main';
+import Elm from './elm';
 import 'phoenix_html';
 
 const elmContainer = document.querySelector('#elm-container');
 if (elmContainer) Elm.Main.embed(elmContainer);
+
+const platformer = document.querySelector("#platformer");
+if (platformer) Elm.Platformer.embed(platformer);
